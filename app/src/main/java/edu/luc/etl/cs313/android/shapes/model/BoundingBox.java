7a08807c82c9fa9d1922a8fec6a5da1e9d095554
Rodrigo Rejas <131706767+rodrigorejas@users.noreply.github.com>
2026-03-18 14:30:45 -0500

    I worked on all necessary parts for BoundingBox.java and Count.java

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -17,24 +17,54 @@ public class BoundingBox implements Visitor<Location> {
 
     @Override
     public Location onFill(final Fill f) {
-        return null;
+        return f.getShape().accept(this);
     }
 
     @Override
     public Location onGroup(final Group g) {
+        if (g.getShapes().isEmpty()) {
+            return new Location(0, 0, new Rectangle(0, 0));
+        }
 
-        return null;
+        Location first = g.getShapes().get(0).accept(this);
+
+        int minX = first.getX();
+        int minY = first.getY();
+        int maxX = minX + ((Rectangle) first.getShape()).getWidth();
+        int maxY = minY + ((Rectangle) first.getShape()).getHeight();
+
+        for (int i = 1; i < g.getShapes().size(); i++) {
+            Location box = g.getShapes().get(i).accept(this);
+
+            int x1 = box.getX();
+            int y1 = box.getY();
+            int x2 = x1 + ((Rectangle) box.getShape()).getWidth();
+            int y2 = y1 + ((Rectangle) box.getShape()).getHeight();
+
+            minX = Math.min(minX, x1);
+            minY = Math.min(minY, y1);
+            maxX = Math.max(maxX, x2);
+            maxY = Math.max(maxY, y2);
+        }
+
+        return new Location(minX, minY,
+                new Rectangle(maxX - minX, maxY - minY));
     }
 
     @Override
     public Location onLocation(final Location l) {
+        Location childBox = l.getShape().accept(this);
 
-        return null;
+        int x = l.getX() + childBox.getX();
+        int y = l.getY() + childBox.getY();
+
+        return new Location(x, y, childBox.getShape());
     }
 
     @Override
     public Location onRectangle(final Rectangle r) {
-        return null;
+        return new Location(0, 0,
+                new Rectangle(r.getWidth(), r.getHeight()));
     }
 
     @Override
@@ -44,11 +74,11 @@ public class BoundingBox implements Visitor<Location> {
 
     @Override
     public Location onOutline(final Outline o) {
-        return null;
+        return o.getShape().accept(this);
     }
 
     @Override
     public Location onPolygon(final Polygon s) {
-        return null;
+        return onGroup(s);
     }
 }
